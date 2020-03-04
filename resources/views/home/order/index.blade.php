ec968e76bd2480d23711b8ace8a95907101bc632
kangfq <86140063@qq.com>
2020-03-04 12:50:20 +0800

    精简订单界面的按钮

--- resources/views/home/order/index.blade.php
+++ resources/views/home/order/index.blade.php
@@ -22,31 +22,41 @@
                         </div>
                         <div class="card-body">
                             <h6 class="card-subtitle mb-4">{{  $order['pro_text'] }}</h6>
-                            <h6 class="card-subtitle mb-2 text-muted">共{{ $order['total_num'] }}件 ￥{{ $order['total_money'] }}</h6>
+                            <h6 class="card-subtitle mb-2 text-muted">共{{ $order['total_num'] }}件
+                                ￥{{ $order['total_money'] }}</h6>
                             <h6 class="card-subtitle mb-2 text-muted">下单时间 {{ $order['created_at'] }}</h6>
                             @if(is_null($order['pay_time']))
                                 <span class="badge badge-warning" style="font-size: 85%!important;">未付款</span>
                             @else
-                                <span class="badge badge-danger" style="margin-right: 5px;font-size: 85%!important;">商家：{{ $order['mall']->name }}</span>
+                                <span class="badge badge-danger"
+                                      style="margin-right: 5px;font-size: 85%!important;">商家：{{ $order['mall']->name }}</span>
                                 <span class="badge badge-success" style="font-size: 85%!important;">已付款</span>
                             @endif
 
                         </div>
                         <div class="card-footer bg-transparent ">
                             @if($order['is_success']===0)
-                                <button type="button" class="btn btn-danger btn-sm" style="float:left;" data-toggle="modal" data-target="#order_pay_{{ $order->id }}">立即付款</button>
-                                <form action="{{ route('order.success',$order['id']) }}" method="post"
-                                      style="float:left;margin-left:10px;">
-                                    @csrf
-                                    <input type="hidden" name="order_id" value="{{ $order['id'] }}">
-                                    <button type="submit" class="btn btn-primary btn-sm">确认收货</button>
-                                </form>
-                                <form action="{{ route('order.destroy',$order['id']) }}" method="post"
-                                      style="float:left;margin-left:10px;">
-                                    @csrf
-                                    @method('delete')
-                                    <button type="submit" class="btn btn-dark btn-sm">删除订单</button>
-                                </form>
+                                @if(is_null($order['pay_time']))
+                                    <button type="button" class="btn btn-danger btn-sm"
+                                            style="float:left;margin-right: 10px;" data-toggle="modal"
+                                            data-target="#order_pay_{{ $order->id }}">立即付款
+                                    </button>
+                                @else
+                                    <form action="{{ route('order.success',$order['id']) }}" method="post"
+                                          style="float:left;margin-right:10px;">
+                                        @csrf
+                                        <input type="hidden" name="order_id" value="{{ $order['id'] }}">
+                                        <button type="submit" class="btn btn-primary btn-sm">确认收货</button>
+                                    </form>
+                                @endif
+                                @if(is_null($order['pay_time']))
+                                    <form action="{{ route('order.destroy',$order['id']) }}" method="post"
+                                          style="float:left;margin-right: 10px;">
+                                        @csrf
+                                        @method('delete')
+                                        <button type="submit" class="btn btn-dark btn-sm">删除订单</button>
+                                    </form>
+                                @endif
                             @else
                                 <button type="button" class="btn btn-success btn-sm" disabled style="cursor: default;">
                                     已经收货
